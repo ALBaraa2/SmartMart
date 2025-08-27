40cb54acf5cf79d5f0d897c2c48a70deb4550a88
ALBaraa <albaraawork3@gmail.com>
2025-08-27 15:45:23 +0300

    Remove unused index on 'status' column in carts migration

--- database/migrations/2025_08_27_120726_create_carts_table.php
+++ database/migrations/2025_08_27_120726_create_carts_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamps();
 
             $table->unique(['user_id', 'supermarket_id']);
-            $table->index('status');
         });
     }
 
