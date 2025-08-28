1d6428a85300b9f3a33d6d42642d84d37d1527de
ALBaraa <albaraawork3@gmail.com>
2025-08-28 12:33:02 +0300

    Refactor Supermarket model to define relationships and fillable attributes

--- app/Models/Supermarket.php
+++ app/Models/Supermarket.php
@@ -2,9 +2,55 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Supermarket extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'location',
+        'logo',
+        'phone',
+        'email',
+        'opening_hours',
+        'status'
+    ];
+
+
+    public function products() {
+        return $this->belongsToMany(Product::class, 'product_supermarket')
+                    ->withPivot('price', 'stock', 'status');
+    }
+
+    public function users() {
+        return $this->belongsToMany(User::class, 'supermarket_user');
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+
+    public function deliveryZones() {
+        return $this->hasMany(DeliveryZone::class);
+    }
+
+    public function deliverySlots() {
+        return $this->hasMany(DeliverySlot::class);
+    }
+
+    public function drivers() {
+        return $this->hasMany(Driver::class);
+    }
+
+    public function settings() {
+        return $this->hasMany(SupermarketSetting::class);
+    }
+
+    public function coupons() {
+        return $this->hasMany(Coupon::class);
+    }
 }
