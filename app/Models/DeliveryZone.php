0662ca049307e3ab47bb21ed9eb5fbb6c5b86258
ALBaraa <albaraawork3@gmail.com>
2025-08-28 13:08:54 +0300

    Add HasFactory trait and define fillable attributes and relationships in DeliveryZone model

--- app/Models/DeliveryZone.php
+++ app/Models/DeliveryZone.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DeliveryZone extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'supermarket_id',
+        'polygon_coordinates',
+        'postal_codes',
+        'min_order_amount',
+        'delivery_fee'
+    ];
+
+    public function supermarket() {
+        return $this->belongsTo(Supermarket::class);
+    }
 }
