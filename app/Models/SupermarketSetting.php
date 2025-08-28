2d93eaf1000d26efa7597a6acbf97d14395e0e68
ALBaraa <albaraawork3@gmail.com>
2025-08-28 13:05:59 +0300

    Add HasFactory trait and define fillable attributes and relationship in SupermarketSetting model

--- app/Models/SupermarketSetting.php
+++ app/Models/SupermarketSetting.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SupermarketSetting extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'supermarket_id',
+        'key',
+        'value'
+    ];
+
+    public function supermarket() {
+        return $this->belongsTo(Supermarket::class);
+    }
 }
