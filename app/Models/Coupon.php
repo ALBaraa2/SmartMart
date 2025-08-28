5de3dfd37b4da5b8d0cb170d674de7f829be920f
ALBaraa <albaraawork3@gmail.com>
2025-08-28 12:55:21 +0300

    Add HasFactory trait and define fillable attributes and relationships in Coupon model

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'discount_type',
+        'discount_value',
+        'usage_limit',
+        'usage_count',
+        'usage_per_user',
+        'start_at',
+        'expires_at'
+    ];
+
+    public function couponUses() {
+        return $this->hasMany(CouponUse::class);
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+
+    public function supermarket() {
+        return $this->belongsTo(Supermarket::class);
+    }
 }
