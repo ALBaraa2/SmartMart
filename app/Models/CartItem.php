d5be956d7f32ec98a4b594527cb75e7f5e20e10d
ALBaraa <albaraawork3@gmail.com>
2025-08-28 12:45:47 +0300

    Add HasFactory trait and define fillable attributes in Cart model

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'cart_id',
+        'product_id',
+        'quantity',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function supermarket() {
+        return $this->belongsTo(Supermarket::class);
+    }
+
+    public function cartItems() {
+        return $this->hasMany(CartItem::class);
+    }
 }
