3b5a0bb55b93a25ebc7e79ee41d9d83cc541028d
ALBaraa <albaraawork3@gmail.com>
2025-08-28 13:10:21 +0300

    Add HasFactory trait and define fillable attributes and relationships in DeliverySlot model

--- app/Models/DeliverySlot.php
+++ app/Models/DeliverySlot.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DeliverySlot extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'supermarket_id',
+        'day',
+        'slot_start',
+        'slot_end'
+    ];
+
+    public function supermarket() {
+        return $this->belongsTo(Supermarket::class);
+    }
+
+    public function deliveries() {
+        return $this->hasMany(Delivery::class);
+    }
 }
