8b44e0123ea18d00ba2704a86e205da33c3b39c5
ALBaraa <albaraawork3@gmail.com>
2025-08-28 13:12:36 +0300

    Add HasFactory trait and define fillable attributes and relationships in Refund model

--- app/Models/Refund.php
+++ app/Models/Refund.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Refund extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'user_id',
+        'amount',
+        'reason',
+        'status',
+        'admin_notes'
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
