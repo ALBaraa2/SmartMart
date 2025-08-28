d0be57564bf73ab881b0715054e88e84326b2c78
ALBaraa <albaraawork3@gmail.com>
2025-08-28 13:07:21 +0300

    Add HasFactory trait and define fillable attributes and relationships in Driver model

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Driver extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'vehicle_type',
+        'vehicle_number',
+        'status',
+        'supermarket_id'
+    ];
+
+    public function supermarket() {
+        return $this->belongsTo(Supermarket::class);
+    }
+
+    public function deliveries() {
+        return $this->hasMany(Delivery::class);
+    }
 }
